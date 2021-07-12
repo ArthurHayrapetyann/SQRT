3d63210a1e717851dbbd27f201dbbdfe1b4acf0f
ArthurHayrapetyann <artur.hayrapetyan.2022@gmail.com>
2021-07-12 11:54:25 +0400

    SQRT

---
+++ sqrt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void sqrt(int a){
+	for (int i = 1; i <=a/2; i++){
+		if (i == a/i){
+			 cout << i;
+			
+		}
+	}
+
+}
+
+
+
+int main(){
+	int k;
+	cin >> k;
+        sqrt(k);
+}
